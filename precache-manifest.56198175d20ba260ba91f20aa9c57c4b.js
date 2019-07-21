669a347b75911dbef74a55c8d7c4759276fa760b
traysimay <traysimay@rumormill.fyi>
2019-07-20 18:42:18 -0700

    Updates

---
+++ precache-manifest.56198175d20ba260ba91f20aa9c57c4b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821e6510ee3bc8b3f764a74f0bf60d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf02c1a2200a3eac5a6",
+    "url": "/static/js/2.d48477b0.chunk.js"
+  },
+  {
+    "revision": "e37f2fabf122d413bd22",
+    "url": "/static/js/main.99410ed7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gify/precache-manifest.3ec0884aacb498fd87e83a85c15f8276.js"
+  "/precache-manifest.56198175d20ba260ba91f20aa9c57c4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
